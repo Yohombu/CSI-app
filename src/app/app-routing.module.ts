0d336d165c9daed35dc6e8bd9ddbf760060606eb
Yohombu Abeysinghe <yohombu.es@gmail.com>
2023-05-19 16:01:14 +0530

    commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GeneralInfoComponent } from './Components/general-info/general-info.component';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'general-info', component:GeneralInfoComponent},
+  { path: '', component:HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { GeneralInfoComponent } from './Components/general-info/general-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    GeneralInfoComponent
   ],
   imports: [
     BrowserModule,
